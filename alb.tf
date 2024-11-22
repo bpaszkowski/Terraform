ace0f0fb750f800e7909b572e21d4bb6433c4cc0
bpaszkowski <bpaszkowski18@gmail.com>
2024-11-22 18:36:39 +0000

    working on my terraform10

--- alb.tf
+++ alb.tf
@@ -4,3 +4,22 @@ resource "aws_default_subnet" "default_az" {
 }
  
 resource "aws_default_vpc" "default" {}
+
+resource "aws_lb" "alb" {
+  name               = "alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.sg_pub.id]
+  subnets            = aws_default_subnet.default_az[*].id
+}
+
+resource "aws_lb_listener" "alb_listener_frontend" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "5000"
+  protocol          = "HTTP"
+ 
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg_frontend.arn
+  }
+}
