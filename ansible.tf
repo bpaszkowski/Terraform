6820383fb80554c9cab1f9ad3386e04e8a08cfa6
bpaszkowski <bpaszkowski18@gmail.com>
2024-11-22 17:26:10 +0000

    working on my terraform3

---
+++ ansible.tf
@@ -0,0 +1,8 @@
+resource "null_resource" "download_ssh_key"{
+    triggers = {
+    timestamp = "${replace("${timestamp()}", "/[-| |T|Z|:]/", "")}"
+}
+provisioner "local-exec" {
+command = "mkdir –p ~/.ssh/ && aws s3 cp s3://${var.bucket_name}/panda_kurs.pem ~/.ssh/panda_kurs.pem || true && chmod 400 ~/.ssh/panda_kurs.pem"
+	}
+}
