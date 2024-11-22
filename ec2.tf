8f85235eb5fa48cef680b12f803bfb08b7df4648
bpaszkowski <bpaszkowski18@gmail.com>
2024-11-22 18:03:54 +0000

    working on my terraform8

--- ec2.tf
+++ ec2.tf
@@ -18,3 +18,26 @@ resource "aws_instance" "panda" {
   vpc_security_group_ids = [aws_security_group.sg_pub.id]
   subnet_id = aws_default_subnet.default_az[count.index].id
 }
+
+resource "aws_security_group" "sg_pub" {
+  ingress {
+    from_port   = 5000
+    to_port     = 5001
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ 
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
