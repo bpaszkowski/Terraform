d9266f49528aec60d9fdfc63ae331c8475862312
bpaszkowski <bpaszkowski18@gmail.com>
2024-11-22 18:38:25 +0000

    working on my terraform11

--- alb.tf
+++ alb.tf
@@ -23,3 +23,11 @@ resource "aws_lb_listener" "alb_listener_frontend" {
     target_group_arn = aws_lb_target_group.tg_frontend.arn
   }
 }
+
+resource "aws_lb_target_group" "tg_frontend" {
+  name        = "tg-frontend"
+  port        = 5000
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = aws_default_vpc.default.id
+}
